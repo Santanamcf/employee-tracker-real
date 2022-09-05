2d1e8859395658e7e556fe68b2e7e8d7df5b0a0b
Santana Fernandez <santanamcf01@gmail.com>
2022-09-05 16:08:52 -0700

    finished update employee feature

--- index.js
+++ index.js
@@ -82,15 +82,15 @@ const viewEmployees = async () => {
         let query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, manager.first_name AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN employee manager ON manager.id = employee.manager_id;';
         connection.query(query, function (err, res) {
             if (err) throw err;
-            let employees = [];
-            res.forEach(employee => employees.push(employee));
-            console.table(employees);
+            let allEmployees = [];
+            res.forEach(employee => allEmployees.push(employee));
+            console.table(allEmployees);
             promptMessages();
         });
     } catch (err) {
         console.log(err);
         promptMessages();
-    };
+    }
 }
 
 const viewDepartments = async () => {
@@ -99,9 +99,26 @@ const viewDepartments = async () => {
         let query = 'SELECT * FROM department';
         connection.query(query, function (err, res) {
             if (err) throw err;
-            let departments = [];
-            res.forEach(department => departments.push(department));
-            console.table(departments);
+            let allDepartments = [];
+            res.forEach(department => allDepartments.push(department));
+            console.table(allDepartments);
+            promptMessages();
+        });
+    } catch (err) {
+        console.log(err);
+        promptMessages();
+    }
+}
+
+const viewRoles = async () => {
+    console.log('Roles');
+    try {
+        let query = 'SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id';
+        connection.query(query, function (err, res) {
+            if (err) throw err;
+            let allRoles = [];
+            res.forEach(role => allRoles.push(role));
+            console.table(allRoles);
             promptMessages();
         });
     } catch (err) {
@@ -109,3 +126,51 @@ const viewDepartments = async () => {
         promptMessages();
     };
 }
+
+const updateEmployee = async () => {
+    try {
+        console.log('Update Employee');
+        
+        let employees = await connection.query("SELECT * FROM employee");
+
+        let selectedEmployee = await inquirer.prompt([
+            {
+                name: 'employee',
+                type: 'list',
+                choices: employees.map((employeeName) => {
+                    return {
+                        name: employeeName.first_name + " " + employeeName.last_name,
+                        value: employeeName.id
+                    }
+                }),
+                message: 'choose an employee to update.'
+            }
+        ]);
+
+        let roles = await connection.query("SELECT * FROM role");
+
+        let selectedRole = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: roles.map((roleName) => {
+                    return {
+                        name: roleName.title,
+                        value: roleName.id
+                    }
+                }),
+                message: 'Update the role of selected employee'
+            }
+        ]);
+
+        let results = await connection.query("UPDATE employee SET ? WHERE ?", [{ role_id: selectedRole.role }, { id: selectedEmployee.employee }]);
+
+        console.log(`The role was successfully updated.`);
+        promptMessages();
+
+    } catch (err) {
+        console.log(err);
+        promptMessages();
+    };
+}
+
